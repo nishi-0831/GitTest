68858ad325044cad41f316383a66fdd38941ee09
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2025-06-10 08:57:38 +0900

    refl-cppのテストに使っている

--- GitTest/FileName.cpp
+++ GitTest/FileName.cpp
@@ -1,7 +1,36 @@
-//これはコメントです
+#include <refl.hpp>
+#include <iostream>
 
-//コミットテスト りんご
+using std::cout;
+using std::endl;
 
-//コミットテスト みかん
+struct Point
+{
+	int x;
+	int y;
+};
 
-//コミットテスト バナナ
+REFL_AUTO(
+	type(Point),
+	field(x),
+	field(y)
+)
+
+int main()
+{
+	constexpr auto type = refl::reflect<Point>();
+	
+	cout << type.name << endl;
+	refl::util::for_each(type.members, [](auto member) 
+		{
+			cout << member.name << endl;
+		}
+	);
+
+	auto members = refl::descriptor::get_members(refl::reflect<Point>());
+	refl::util::for_each(members, [](auto member)
+	{
+			cout << member.name << endl;
+	}
+		);
+}
